38f6ba33938261692776bed5a8180c3750bc32f2
alex_lee <>
2024-10-22 09:18:16 +0800

    chore: 引入代码提交规范

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  // ... 其他配置
-};
+    experimental: {
+        // appDir: true,
+    },
+    // ... 其他配置
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
